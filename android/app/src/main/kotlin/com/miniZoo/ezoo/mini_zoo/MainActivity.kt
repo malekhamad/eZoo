007e886082225306305fcd0efd4ba759a2bb451f
malekhamad <malek.hamad@maf.ae>
2023-03-01 13:15:00 +0300

    structure project

---
+++ android/app/src/main/kotlin/com/miniZoo/ezoo/mini_zoo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.miniZoo.ezoo.mini_zoo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,11 @@
+class AppConstants{
+
+  /// Shared Preference Keys ...
+  static String isFirstTimeOpened = "is_first_time_opened";
+  static String isLightMode ="is_light_mode";
+  static String email = "email";
+  static String password = "password";
+  static String isUser = "is_user";
+
+
+}
